df68f5a10c01ba5d4a51e49a54abccb448d40daf
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2018-05-01 13:13:34 -0400

    add column generation for generalized assignment problem

--- src/StudentHousing.jl
+++ src/StudentHousing.jl
@@ -7,10 +7,12 @@ using StatsBase, Distributions,
 
 export
     StudentHousingData,
-    onestagemodel, multistagemodel
+    onestagemodel, multistagemodel,
+    house_allowedby, house_fits_pattern, beds_needed, beds_avail
 
 include("housing/binary_hacks.jl")
 include("housing/data.jl")
 include("housing/model.jl")
+include("GAP/gap.jl")
 
 end # module
